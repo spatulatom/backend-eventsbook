0e5304bfe6bce7cf5ac1e6dc8511edbcb88fe7af
spatulatom <spatulatom@gmail.com>
2022-06-13 11:02:31 +0100

    commit

--- util/location.js
+++ util/location.js
@@ -15,7 +15,7 @@ async function getCoordsForAddress(address) {
   const response = await axios.get(
     `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
       address
-    // )}&key=${process.env.GOOGLE_API_KEY}`
+    )}&key=${process.env.GOOGLE_API_KEY}`
     
   );
     const data = response.data;

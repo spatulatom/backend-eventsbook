1fec959b09d3ed75541be4f140b1b06a939dec00
spatulatom <spatulatom@gmail.com>
2023-02-11 19:37:46 +0000

    url encoded

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -42,20 +44,21 @@ app.use((req, res, next) => {
 });
 
 app.use((error, req, res, next) => {
-  console.log('ERRORR', error)
+  console.log('ERRORR', error);
   if (error instanceof MulterError) {
     // https://github.com/expressjs/multer/issues/602
     // note that 'invalid meme type' is still handled separetly and is not catched here
     // as MulterError, gies straight to the bottom
     error.code = 413;
-    error.message = "Image too large, max size is 10mb! Upload smaller image please.";
-    }
+    error.message =
+      'Image too large, max size is 10mb! Upload smaller image please.';
+  }
   if (req.file) {
-    fs.unlink(req.file.path, err => {
+    fs.unlink(req.file.path, (err) => {
       console.log(err);
     });
   }
-  // So when you add a custom error handler, you must delegate to the default Express error handler, when 
+  // So when you add a custom error handler, you must delegate to the default Express error handler, when
   // the headers have already been sent to the client:
   if (res.headerSent) {
     return next(error);
@@ -69,8 +72,8 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@freecluster.qqg7h.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(process.env.PORT||5000);
+    app.listen(process.env.PORT || 5000);
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(err);
   });

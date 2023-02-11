4d81300a7f66df9632536bef5a696aa2ccfddad2
spatulatom <spatulatom@gmail.com>
2023-02-11 22:05:02 +0000

    kjkjkj

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -7,21 +7,26 @@ const MIME_TYPE_MAP = {
   'image/jpg': 'jpg'
 };
 const maxSize = 1 * 1000 * 10000;
-const fileUpload = multer({
+const fileUpload = multer(
+  {
   // limits: 500000,
   limits:{
     fileSize: maxSize},
+   
     
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
+      console.log("multer1");
       cb(null, 'uploads/images');
     },
     filename: (req, file, cb) => {
+      console.log("multer2");
       const ext = MIME_TYPE_MAP[file.mimetype];
       cb(null, uuid() + '.' + ext);
     }
   }),
   fileFilter: (req, file, cb) => {
+    console.log("multer3");
     const isValid = !!MIME_TYPE_MAP[file.mimetype];
     // we are not throwing an error here yet with the icorrect mime type the bleow message
     // was show on the frontend, perhaps 'cb' throws

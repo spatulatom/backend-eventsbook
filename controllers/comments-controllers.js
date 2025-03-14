0c0cae6fb693178e09ca5def3b5cef71aa5c89f3
spatulatom <spatulatom@gmail.com>
2025-03-14 13:53:56 +0000

    refactor createComment function for improved error handling and code clarity

--- controllers/comments-controllers.js
+++ controllers/comments-controllers.js
@@ -1,95 +1,81 @@
-const { validationResult } = require('express-validator');
-const mongoose = require('mongoose');
-const {v1} = require('uuid');
+const { validationResult } = require("express-validator");
+const mongoose = require("mongoose");
+const { v1 } = require("uuid");
 
+const HttpError = require("../models/http-error");
+const User = require("../models/user");
+const Place = require("../models/event");
 
-const HttpError = require('../models/http-error');
-const User = require('../models/user');
-const Place = require('../models/event')
-
-
-
-const createComment = async(req, res, next)=>{
-
-
+const createComment = async (req, res, next) => {
   const errors = validationResult(req);
-    
+
   if (!errors.isEmpty()) {
     return next(
-      new HttpError('Invalid inputs passed, please check your data.', 422)
+      new HttpError("Invalid inputs passed, please check your data.", 422)
     );
   }
 
-    let user;
-                   try {
-                     user = await User.findById(req.userData.userId);
-                   } catch (err) {
-                     const error = new HttpError(
-                       'Creating comment failed, please try again.',
-                       500
-                     );
-                     
-                     return next(error);
-                   }
-                 
-                   if (!user) {
-                     const error = new HttpError('Could not find user for provided id.', 404);
-                     return next(error);
-                   }
+  let user;
+  try {
+    user = await User.findById(req.userData.userId);
+  } catch (err) {
+    const error = new HttpError(
+      "Creating comment failed, please try again.",
+      500
+    );
 
-    let place;
-    try {
-        place = await Place.findById(req.body.placeId);
-      } catch (err) {
-        const error = new HttpError(
-          'Creating comment failed 2, please try again.',
-          500
-        );
-        return next(error);
-      }
-    
-      if (!place) {
-          const error = new HttpError('Could not find place for provided id.', 404);
-          return next(error);
-          }
+    return next(error);
+  }
 
+  if (!user) {
+    const error = new HttpError("Could not find user for provided id.", 404);
+    return next(error);
+  }
 
-          let date;
-          let localDate;
-          // heroku time is -1h, so temporarly we will add 1h
-          // date = new Date();
-          date = new Date(new Date().setHours(new Date().getHours() + 1));
-         // Specify date and time format using "style" options (i.e. full, long, medium, short)
-          localDate = new Intl.DateTimeFormat('en-GB',{ dateStyle: 'medium', timeStyle: 'short' }).format(date);
+  let place;
+  try {
+    place = await Place.findById(req.body.placeId);
+  } catch (err) {
+    const error = new HttpError(
+      "Creating comment failed 2, please try again.",
+      500
+    );
+    return next(error);
+  }
 
+  if (!place) {
+    const error = new HttpError("Could not find place for provided id.", 404);
+    return next(error);
+  }
 
-                   let comment = {
-                       description: req.body.description,
-                       addedBy: user.name,
-                       date: localDate,
-                       id: v1()
-                    }
+  let date;
+  let localDate;
+  // heroku time is -1h, so temporarly we will add 1h
+  // date = new Date();
+  date = new Date(new Date().setHours(new Date().getHours() + 1));
+  // Specify date and time format using "style" options (i.e. full, long, medium, short)
+  localDate = new Intl.DateTimeFormat("en-GB", {
+    dateStyle: "medium",
+    timeStyle: "short",
+  }).format(date);
 
-                    place.comments.push(comment);
+  let comment = {
+    description: req.body.description,
+    addedBy: user.name,
+    date: localDate,
+    id: v1(),
+  };
 
+  place.comments.push(comment);
 
+  try {
+    await place.save();
+  } catch (err) {
+    const error = new HttpError("Adding comment failed.", 500);
+    return next(error);
+  }
 
-                  try {
-                    await place.save();
-                  } catch (err) {
-                    const error = new HttpError(
-                      'Adding comment failed.',
-                      500
-                    );
-                    return next(error);
-                  }
-          
-                  res
-                  .status(201)
-                  .json(comment);
-
-              
-
-}
+  res.status(201).json(comment);
+};
 
 exports.createComment = createComment;
